63b748213ef25f62d0530e45beb2dc7e8473fd13
aagarcia25 <109979605+aagarcia25@users.noreply.github.com>
2023-09-25 12:21:41 -0600

    se cambia icono y se agrega numero de oficio en visualizar documentos

--- src/app/views/componentes/VisorDocumentos.tsx
+++ src/app/views/componentes/VisorDocumentos.tsx
@@ -3,6 +3,7 @@ import DownloadingIcon from "@mui/icons-material/Downloading";
 import FileUploadIcon from "@mui/icons-material/FileUpload";
 import RemoveRedEyeIcon from "@mui/icons-material/RemoveRedEye";
 import {
+  Box,
   DialogContent,
   Grid,
   IconButton,
@@ -365,6 +366,7 @@ const VisorDocumentos = ({
   };
 
   useEffect(() => {
+    console.log(obj);
     permisos.map((item: PERMISO) => {
       if (String(item.ControlInterno) === "AUDITOR") {
         if (String(item.Referencia) === "AGREG") {
@@ -385,6 +387,10 @@ const VisorDocumentos = ({
     <div>
       <ModalForm title={"Visor de Documentos"} handleClose={handleFunction}>
         <Progress open={openSlider}></Progress>
+
+        <Box sx={{ display: "flex", justifyContent: "center" }}>
+          <Typography variant="h4">{obj.row.Oficio}</Typography>
+        </Box>
 
         {true ? (
           <>
