0bf49cb780d323edfea5c8e6b6b566df5ec481a7
aagarcia25 <109979605+aagarcia25@users.noreply.github.com>
2024-06-25 17:29:37 -0600

    Update ControlOficios.tsx

--- src/app/views/ControlOficios/ControlOficios.tsx
+++ src/app/views/ControlOficios/ControlOficios.tsx
@@ -64,7 +64,8 @@ export const ControlOficios = () => {
   const [cancelar, setCancelar] = useState<boolean>(false);
   const [bs, setBs] = useState<boolean>(false);
   const [show, setShow] = useState(false);
-
+  const [countFiles, setCountFiles] = useState<any[]>([]);
+  const [data, setData] = useState([]);
   const handleAccion = (v: any) => {
     Swal.fire({
       icon: "info",
@@ -552,6 +553,9 @@ export const ControlOficios = () => {
               icon={<AttachmentIcon />}
               row={v}
             ></ButtonsDetail>
+            {countFiles.length > 0
+              ? countFiles.find((obj) => obj.id == v.row.id)?.data
+              : "Validando"}
             {editar ? (
               <ButtonsEdit
                 handleAccion={handleEdit}
@@ -690,6 +694,98 @@ export const ControlOficios = () => {
         Swal.fire("¡Error!", res.STRMESSAGE, "error");
       }
     });
+  };
+
+  const consultaArchivos = (Ruta: string): Promise<any> => {
+    return new Promise((resolve, reject) => {
+      setOpenSlider(true);
+
+      let data = {
+        NUMOPERACION: 16,
+        TOKEN: JSON.parse(String(getToken())),
+        RUTA: Ruta,
+      };
+
+      AuditoriaService.FoliosFilesindex(data)
+        .then((res) => {
+          setOpenSlider(false);
+          if (res.SUCCESS) {
+            const response = res.RESPONSE;
+            // Contar la cantidad de archivos
+            const fileCount = Array.isArray(response) ? response.length : 0;
+            response.fileCount = fileCount;
+
+            // Toast.fire({
+            //   icon: "success",
+            //   title: "¡Consulta Exitosa!",
+            // });
+            setData(response);
+            console.log("res.RESPONSE", response);
+            resolve(response);
+          } else {
+            Swal.fire("¡Error!", res.STRMESSAGE, "error");
+            reject(new Error(res.STRMESSAGE));
+          }
+        })
+        .catch((error) => {
+          setOpenSlider(false);
+          reject(error);
+        });
+    });
+  };
+
+  const procesarRutasConLimite = async (
+    rutas: { id: string; ruta: string }[],
+    concurrencyLimit: number
+  ): Promise<any[]> => {
+    const resultados: any[] = [];
+    const ejecutar = async (ruta: { id: string; ruta: string }) => {
+      try {
+        const resultado = await consultaArchivos(ruta.ruta);
+        console.log("resultado", resultado.length);
+
+        resultados.push({
+          ruta: ruta.ruta,
+          data: resultado.length,
+          id: ruta.id,
+        });
+      } catch (error) {
+        resultados.push({ ruta: ruta.ruta, data: "Validando...", id: ruta.id });
+      }
+    };
+
+    const ejecutarConLimite = async () => {
+      const cola = [...rutas];
+      const procesos: Promise<void>[] = [];
+
+      for (let i = 0; i < concurrencyLimit; i++) {
+        const ruta = cola.shift();
+        if (ruta) {
+          procesos.push(ejecutar(ruta));
+        }
+      }
+
+      while (cola.length > 0) {
+        await Promise.any(procesos);
+        const ruta = cola.shift();
+        if (ruta) {
+          procesos.push(ejecutar(ruta));
+        }
+      }
+
+      await Promise.all(procesos);
+    };
+
+    await ejecutarConLimite();
+    return resultados;
+  };
+
+  const iniciar = async (auxRutas: { id: string; ruta: string }[]) => {
+    const concurrencyLimit = 10; // Ajusta este valor según las capacidades de tu servidor
+
+    const resultados = await procesarRutasConLimite(auxRutas, concurrencyLimit);
+    console.log("resultados", resultados);
+    setCountFiles(resultados);
   };
 
   useEffect(() => {
